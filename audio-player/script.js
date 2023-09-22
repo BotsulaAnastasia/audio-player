306ef89775ded277d8833baecd247a06aa98509f
Anastasia Botsula <nactya.botsula@gmail.com>
2023-09-22 22:18:38 +0300

    script: get DOM elements

---
+++ audio-player/script.js
@@ -0,0 +1,15 @@
+const player = document.querySelector('.player-wrapper'),
+        audio = document.querySelector('.audio'),
+        cover = document.querySelector('.cover-img'),
+        progressWrapper = document.querySelector('.progress-wrapper'),
+        progress = document.querySelector('.progress'),
+        songArtist = document.querySelector('.song-artist'),
+        songTitle = document.querySelector('.song-title'),
+        playBtn = document.querySelector('.play-btn'),
+        playOrPause = document.querySelector('.play_pause'),
+        prevBtn = document.querySelector('.prev-song-btn'),
+        nextBtn = document.querySelector('.next-song-btn'),
+        currentTime = document.querySelector('.current-time'),
+        totalTime = document.querySelector('.total-time');
+        
+        
